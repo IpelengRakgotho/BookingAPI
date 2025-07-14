7177c87bc09d1cb3be8dd3d1f323f52d6a7c01b5
Lerato <lerato.rakgotho@gmail.com>
2025-07-14 15:55:15 +0200

    Adding Entities

---
+++ PsiraWebApi/Entities/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceBookingSystemAPI.Entities
+{
+    public class Booking
+    {
+        [Key]
+        public int BookingId { get; set; }
+        public int ResourceId { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public string? BookedBy { get; set; }
+        public string? Purpose { get; set; }
+
+    }
+}
---
+++ PsiraWebApi/Entities/Resource.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceBookingSystemAPI.Entities
+{
+    public class Resource
+    {
+        [Key]
+        public int ResourceId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public int? Capacity { get; set; }
+        public bool? IsAvailable { get; set; }
+
+
+    }
+}
